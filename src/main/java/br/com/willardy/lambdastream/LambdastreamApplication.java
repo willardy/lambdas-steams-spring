41f69d53360e2eb2ecba33c4f9be18ce9a864eac
Willardy <willardy.oliveira@solutis.com.br>
2024-02-02 11:04:04 -0300

    Criando serviço de consumo de Api e mostrando resultado no terminal

--- src/main/java/br/com/willardy/lambdastream/LambdastreamApplication.java
+++ src/main/java/br/com/willardy/lambdastream/LambdastreamApplication.java
@@ -1,13 +1,25 @@
 package br.com.willardy.lambdastream;
 
+import br.com.willardy.lambdastream.local.service.ConsumoAPIService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication
-public class LambdastreamApplication {
+public class LambdastreamApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(LambdastreamApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        var consumoApi = new ConsumoAPIService();
+        var json = consumoApi.obterDados("https://www.omdbapi.com/?t=Transform&apikey=8d1c9917");
+
+        Logger logger = Logger.getLogger(getClass().getName());
+        logger.info(json);
+    }
 }
